a21265937c346fcc070222ca61a541ab6cb4730c
Gerson Hugo <gersonhugo@gmail.com>
2023-01-24 19:10:16 -0300

    resolvido exercícios de arrays e for

---
+++ fundamentos/secao-3-introducao-a-javascript/dia-2-javascript-array-e-loop-for/scripts2.js
@@ -0,0 +1,11 @@
+let cars = ['Saab', 'Volvo', 'BMW'];
+
+for (let index = 0; index < cars.length; index += 1) {
+  console.log(cars[index]);
+}
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let i = 0; i < groceryList.length; i += 1) {
+    console.log(groceryList[i])
+}
