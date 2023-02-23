c1fd615c6dd72d277d2277ff9db627b84f6a6247
Gerson Hugo <gersonhugo@gmail.com>
2023-02-23 20:25:01 -0300

    desenvolvidos mais 2 testes para o exercício 1

--- fundamentos/secao-6-introducao-a-javascript-es6-e-testes-unitarios/dia-2-primeiros-passos-em-jest/exercicio1.test.js
+++ fundamentos/secao-6-introducao-a-javascript-es6-e-testes-unitarios/dia-2-primeiros-passos-em-jest/exercicio1.test.js
@@ -3,3 +3,11 @@ const myRemove = require('./exercicio1');
 test('verifica se o elemento "item" está sendo removido do array', () => {
     expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
   });
+
+test('verifica se a função não retorna [1, 2, 3, 4]', () => {
+  expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+});
+
+test('verifica se a função retorna [1, 2, 3, 4]', () => {
+  expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+});
