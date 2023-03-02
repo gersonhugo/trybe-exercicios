c646e947bd4d5f06f81a4f380ac369008650381d
Gerson Hugo <gersonhugo@gmail.com>
2023-03-02 15:31:41 -0300

    resolvido exemplo de forEach

---
+++ fundamentos/secao-7-higher-order-functions-do-javascript-es6/dia-1-introducao-a-higher-order-functions/forEach.js
@@ -0,0 +1,11 @@
+const emailListInData = [
+    'roberta@email.com',
+    'paulo@email.com',
+    'anaroberta@email.com',
+    'fabiano@email.com',
+];
+
+// Adicione seu código aqui
+const emails = emailListInData.forEach(element => {
+    console.log(element);;
+});
