26e90be3dc56f2079f9944f2f2d326867df55370
Gerson Hugo <gersonhugo@gmail.com>
2023-03-28 18:17:38 -0300

    criada primeira aplicação com react

---
+++ front-end/secao-2-introducao-ao-react/dia-1-hello-world-no-react/my-app/src/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class About extends React.Component {
+    render () {
+        return (
+            <div>
+            <h1>Gerson Hugo</h1>
+            <p>Tenho 33 anos, gosto de computadores e música.</p>
+            <h2>Minhas Habilidades</h2>
+            <ol>
+                <li>Tocar guitarra</li>
+                <li>Formatar computadores</li>
+                <li>Montar cubo mágico</li>
+            </ol>
+        </div> 
+        );
+    };
+};
+
+export default About;
