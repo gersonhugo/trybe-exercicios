49198653dbfaea14fcf9df31b90f87a5b91b94c1
Gerson Hugo <gersonhugo@gmail.com>
2023-04-03 15:52:43 -0300

    funções declaradas dentro da classe, e possuem acesso ao objeto this

--- front-end/secao-3-componentes-com-estado-eventos-e-formularios-em-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/secao-3-componentes-com-estado-eventos-e-formularios-em-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
 
-function handleClick() {
-  console.log('texto qualquer');
-}
-
-function handleClick2() {
-  console.log('texto qualquer 2');
-}
-
-function handleClick3() {
-  console.log('texto qualquer 3');
-}
-
 class App extends React.Component {
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this)
+    this.handleClick2 = this.handleClick2.bind(this)
+  }
+
+  handleClick() {
+    console.log('texto qualquer');
+    console.log(this);
+  }
+  
+  handleClick2() {
+    console.log('texto qualquer 2');
+    console.log(this);
+  }
+  
+  handleClick3 = () => {
+    console.log('texto qualquer 3');
+    console.log(this);
+  }
   render() {
+
     return (
       <div>
-        <button onClick={handleClick}>Imprimir texto</button>
-        <button onClick={handleClick2}>Imprimir texto</button>
-        <button onClick={handleClick3}>Imprimir texto</button>
+        <button onClick={this.handleClick}>Imprimir texto</button>
+        <button onClick={this.handleClick2}>Imprimir texto</button>
+        <button onClick={this.handleClick3}>Imprimir texto</button>
       </div>
     )
   }
