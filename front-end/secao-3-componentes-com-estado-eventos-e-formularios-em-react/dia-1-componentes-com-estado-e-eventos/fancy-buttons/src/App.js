3cb38540eef5d008ed734a3335f7d62f128e9024
Gerson Hugo <gersonhugo@gmail.com>
2023-04-03 16:27:12 -0300

    app atualizada para contabilizar o número de cliques através do state

--- front-end/secao-3-componentes-com-estado-eventos-e-formularios-em-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/secao-3-componentes-com-estado-eventos-e-formularios-em-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -5,29 +5,40 @@ class App extends React.Component {
     super()
     this.handleClick = this.handleClick.bind(this)
     this.handleClick2 = this.handleClick2.bind(this)
+    this.state = {
+      numeroDeCliques1: 0,
+      numeroDeCliques2: 0,
+      numeroDeCliques3: 0,
+    }
   }
 
   handleClick() {
-    console.log('texto qualquer');
-    console.log(this);
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques1: estadoAnterior.numeroDeCliques1 + 1
+    }))
   }
   
   handleClick2() {
-    console.log('texto qualquer 2');
-    console.log(this);
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques2: estadoAnterior.numeroDeCliques2 + 1
+    }))
   }
   
   handleClick3 = () => {
-    console.log('texto qualquer 3');
-    console.log(this);
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques3: estadoAnterior.numeroDeCliques3 + 1
+    }))
   }
   render() {
-
+    const {numeroDeCliques1} = this.state;
+    const {numeroDeCliques2} = this.state;
+    const {numeroDeCliques3} = this.state;
+    
     return (
       <div>
-        <button onClick={this.handleClick}>Imprimir texto</button>
-        <button onClick={this.handleClick2}>Imprimir texto</button>
-        <button onClick={this.handleClick3}>Imprimir texto</button>
+        <button onClick={this.handleClick}>{numeroDeCliques1}</button>
+        <button onClick={this.handleClick2}>{numeroDeCliques2}</button>
+        <button onClick={this.handleClick3}>{numeroDeCliques3}</button>
       </div>
     )
   }
